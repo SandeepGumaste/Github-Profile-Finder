ee7511914d9ebf37b9a0e8a4fffb99d9105def0e
Sandeep Gumaste <sandeepgumaste10@gmail.com>
2022-07-11 02:05:19 +0530

    added user page

--- src/components/layout/Spinner.jsx
+++ src/components/layout/Spinner.jsx
@@ -2,7 +2,7 @@ import { ImSpinner9 } from "react-icons/im";
 
 const Spinner = () => {
   return (
-    <div className="w-full mt-20 animate-bounce">
+    <div className="w-full mt-20">
       <ImSpinner9 className="ml-auto mr-auto w-16 h-16 animate-spin" />
     </div>
   );
--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -6,6 +6,20 @@ const githubReducer = (state, action) => {
         users: action.payload,
         isLoading: false,
       };
+    case "GET_USER_AND_REPOS":
+      return {
+        ...state,
+        user: action.payload.user,
+        repos: action.payload.repos,
+        isLoading: false,
+      };
+
+    case "CLEAR_USERS":
+      return {
+        ...state,
+        users: [],
+        isLoading: false,
+      };
     case "SET_ISLOADING":
       return {
         ...state,
